666d1aa6afb55cfd49e0e323e02e2985cacefe75
yaltha <yaltharullist@gmail.com>
2021-01-29 22:23:11 +0700

    Initiate the files -  components Form and Result

---
+++ src/components/Form.js
@@ -0,0 +1,10 @@
+const Form = () => {
+    return (
+        <div>
+            <h1>Form Field</h1>  
+                      
+        </div>
+    )
+}
+
+export default Form
---
+++ src/components/Result.js
@@ -0,0 +1,9 @@
+const Result = () => {
+    return (
+        <div>
+            <h1>Result Field</h1>
+        </div>
+    )
+}
+
+export default Result
